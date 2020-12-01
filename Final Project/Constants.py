4d9bab153a5e6bae850364830a19b14ef7f90424
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-01 19:04:51 +0100

    Converted colour constants enum class

--- Final Project/Constants.py
+++ Final Project/Constants.py
@@ -1,17 +1,11 @@
 from typing import Tuple
 
 # Window Constants
-WIDTH: int = 500
-HEIGHT: int = 600
+WINDOW_WIDTH: int = 500
+WINDOW_HEIGHT: int = 600
 
 # Grid Constants
 GRID_WIDTH: int = 10
 GRID_HEIGHT: int = 10
 CELL_DIMENSION: int = 20
-
-# Colours
-WHITE: Tuple[int, int, int] = (255, 255, 255)
-RED: Tuple[int, int, int] = (255, 0, 0)
-BLUE: Tuple[int, int, int] = (0, 0, 255)
-YELLOW: Tuple[int, int, int] = (255, 255, 0)
 
