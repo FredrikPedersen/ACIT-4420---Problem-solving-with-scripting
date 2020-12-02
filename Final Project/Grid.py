d295591a5d30fe8b7eaf81f7273f362f71f267bf
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-02 17:01:48 +0100

    Renamed CELL_DIMENSION constant to CELL_SIZE

--- Final Project/Grid.py
+++ Final Project/Grid.py
@@ -7,12 +7,12 @@ def generate_grid() -> List[Tuple[int, int]]:
     y: int = 0
 
     for i in range(0, GRID_HEIGHT):
-        x: int = CELL_DIMENSION
-        y += CELL_DIMENSION
+        x: int = CELL_SIZE
+        y += CELL_SIZE
         for j in range(0, GRID_WIDTH):
 
             grid.append((x, y))
-            x += CELL_DIMENSION
+            x += CELL_SIZE
 
     return grid
 
