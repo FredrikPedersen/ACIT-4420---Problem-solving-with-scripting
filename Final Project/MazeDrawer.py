1c815a58d5b831ae0bac4bdef9346b748b88ec9b
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-02 17:02:08 +0100

    Cleanup in functions, added documentation. Renamed CELL_DIMENSION constant to CELL_SIZE.

--- Final Project/MazeDrawer.py
+++ Final Project/MazeDrawer.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import random
-from typing import Tuple, List, Dict, Union
+from typing import Tuple, List, Dict, Union, Set
 from pygame.surface import SurfaceType, Surface
 from Grid import generate_grid
 from Constants import *
@@ -13,7 +13,8 @@ class MazeDrawer:
 
     __rootX: int = 20
     __rootY: int = 20
-    __solutionStart: int = GRID_WIDTH * GRID_HEIGHT
+    __solutionStartX: int = GRID_WIDTH * CELL_SIZE
+    __solutionStartY: int = GRID_HEIGHT * CELL_SIZE
     __grid: List[Tuple[int, int]] = generate_grid()
     __recursiveSolution: Dict = {}
 
@@ -24,10 +25,9 @@ class MazeDrawer:
         """
         Public facing convenience function for performing all drawing tasks of this class
         """
-        time.sleep(5)
         self.__draw_grid()
-        self.__draw_maze(self.__rootX, self.__rootY)
-        self.__draw_solution()
+        self.__draw_maze()
+        self.__draw_recursive_solution()
 
     def __draw_grid(self) -> None:
         """
@@ -38,10 +38,10 @@ class MazeDrawer:
             x: int = coordinates[0]
             y: int = coordinates[1]
 
-            pygame.draw.line(self.__screen, Colour.BLACK.value, [x, y], [x + CELL_DIMENSION, y])
-            pygame.draw.line(self.__screen, Colour.BLACK.value, [x + CELL_DIMENSION, y + CELL_DIMENSION], [x, y + CELL_DIMENSION])
-            pygame.draw.line(self.__screen, Colour.BLACK.value, [x + CELL_DIMENSION, y], [x + CELL_DIMENSION, y + CELL_DIMENSION])
-            pygame.draw.line(self.__screen, Colour.BLACK.value, [x, y + CELL_DIMENSION], [x, y])
+            pygame.draw.line(self.__screen, Colour.BLACK.value, [x, y], [x + CELL_SIZE, y])
+            pygame.draw.line(self.__screen, Colour.BLACK.value, [x + CELL_SIZE, y + CELL_SIZE], [x, y + CELL_SIZE])
+            pygame.draw.line(self.__screen, Colour.BLACK.value, [x + CELL_SIZE, y], [x + CELL_SIZE, y + CELL_SIZE])
+            pygame.draw.line(self.__screen, Colour.BLACK.value, [x, y + CELL_SIZE], [x, y])
             pygame.display.update()
 
     def __draw_maze_cell(self, x, y, direction: Direction = None, colour: Colour = None) -> None:
@@ -55,7 +55,7 @@ class MazeDrawer:
         """
 
         # Offsets to make sure the maze's tile colour does not fill over the wall colour.
-        rectangle_size: int = CELL_DIMENSION - 1
+        rectangle_size: int = CELL_SIZE - 1
         x += 1
         y += 1
 
@@ -68,7 +68,7 @@ class MazeDrawer:
             pygame.draw.rect(self.__screen, colour_value, (x, y, rectangle_size, rectangle_size), 0)
 
         if direction == Direction.LEFT:
-            pygame.draw.rect(self.__screen, colour_value, (x - CELL_DIMENSION, y, rectangle_size * 2, rectangle_size), 0)
+            pygame.draw.rect(self.__screen, colour_value, (x - CELL_SIZE, y, rectangle_size * 2, rectangle_size), 0)
 
         if direction == Direction.RIGHT:
             pygame.draw.rect(self.__screen, colour_value, (x, y, rectangle_size * 2, rectangle_size), 0)
@@ -77,58 +77,92 @@ class MazeDrawer:
             pygame.draw.rect(self.__screen, colour_value, (x, y, rectangle_size, rectangle_size * 2), 0)
 
         if direction == Direction.UP:
-            pygame.draw.rect(self.__screen, colour_value, (x, y - CELL_DIMENSION, rectangle_size, rectangle_size * 2), 0)
+            pygame.draw.rect(self.__screen, colour_value, (x, y - CELL_SIZE, rectangle_size, rectangle_size * 2), 0)
 
         pygame.display.update()
 
-    def __draw_backtracking_cell(self, x, y):
-        rectangle_size: int = CELL_DIMENSION - 1
+    def __draw_backtracking_cell(self, x, y) -> None:
+        """
+        Draws a red square at position (x, y), then changes the colour back to default after .05 seconds.
+        This creates a blinking red cell, used for displaying how the maze-algorithm backtracks through the stack.
+
+        :param x: x-coordinate to draw red cell at
+        :param y: y-coordinate to draw red cell at
+        """
+
+        # Offsets to make sure the maze's tile colour does not fill over the wall colour.
+        rectangle_size: int = CELL_SIZE - 1
 
         pygame.draw.rect(self.__screen, Colour.RED.value, (x + 1, y + 1, rectangle_size, rectangle_size), 0)
         pygame.display.update()
 
-    def __draw_solution_cell(self, x, y):
-        # Offset to place the circle in the center of the cell
-        x += CELL_DIMENSION / 2
-        y += CELL_DIMENSION / 2
+        time.sleep(.05)
 
+        # Change colour back after the backtracking has been displayed
+        self.__draw_maze_cell(x, y)
+        pygame.display.update()
+
+    def __draw_solution_cell(self, x, y) -> None:
+        """
+        Draws a red circle in the center of the cell at position (x, y).
+        Used to draw individual steps in the solution path.
+
+        :param x: x-coordinate to draw circle on
+        :param y: x-coordinate to draw circle on
+        """
+
+        # Add an offset to place the circle in the center of the cell
+        x += CELL_SIZE / 2
+        y += CELL_SIZE / 2
         pygame.draw.circle(self.__screen, Colour.RED.value, (x, y), 3)
         pygame.display.update()
 
-    def __find_unvisited_neighbours(self, x: int, y: int, visited: List[Tuple[int, int]]) -> List[Direction]:
+    def __find_unvisited_neighbours(self, x: int, y: int, visited: Set[Tuple[int, int]]) -> List[Direction]:
+        """
+        Finds all the existing unvisited neighbour for the current cell (x, y).
+
+        :param x: x-coordinate of current cell.
+        :param y: y-coordinate of current cell.
+        :param visited: List of all visited cells in the current maze.
+        :return: List of the direction of all unvisited neighbours of the current cell (x, y).
+        """
+
         neighbouring_cells: List[Direction] = []
 
         # Check if right, left, bottom and top cells are already visited and exists, respectively.
-        if (x + CELL_DIMENSION, y) not in visited and (x + CELL_DIMENSION, y) in self.__grid:
+        if (x + CELL_SIZE, y) not in visited and (x + CELL_SIZE, y) in self.__grid:
             neighbouring_cells.append(Direction.RIGHT)
 
-        if (x - CELL_DIMENSION, y) not in visited and (x - CELL_DIMENSION, y) in self.__grid:
+        if (x - CELL_SIZE, y) not in visited and (x - CELL_SIZE, y) in self.__grid:
             neighbouring_cells.append(Direction.LEFT)
 
-        if (x, y + CELL_DIMENSION) not in visited and (x, y + CELL_DIMENSION) in self.__grid:
+        if (x, y + CELL_SIZE) not in visited and (x, y + CELL_SIZE) in self.__grid:
             neighbouring_cells.append(Direction.DOWN)
 
-        if (x, y - CELL_DIMENSION) not in visited and (x, y - CELL_DIMENSION) in self.__grid:
+        if (x, y - CELL_SIZE) not in visited and (x, y - CELL_SIZE) in self.__grid:
             neighbouring_cells.append(Direction.UP)
 
         return neighbouring_cells
 
-    def __draw_maze(self, x, y):
+    def __draw_maze(self):
         """
-        Recursive randomized depth-first search to create a maze on an nxn grid of cells.
+        Recursive randomized depth-first search to create a maze on a grid of cells.
         For each step of the algorithm:
             1. Mark the current cell as visited
             2. While the current cell has any unvisited neighbour cells:
-                 - Choose on of the unvisited neighbours.
+                 - Choose one of the unvisited neighbours.
                  - Remove the wall between the current cell and the chosen neighbouring cell.
                  - Invoke the routine for the chosen neighbouring cell.
 
-        :param x: X-coordinate of starting point
-        :param y: Y-coordinate of starting point
+        Also creates a recursive solution for the maze by keeping track of the current cell and what was the previous
+        step in a key-value pair, with key being the previous cell, and value being the current cell.
         """
 
+        x = self.__rootX
+        y = self.__rootY
+
         # Mark the starting location as visited, add it to the stack and draw it.
-        visited: List[Tuple[int, int]] = [(x, y)]
+        visited: Set[Tuple[int, int]] = {(x, y)}
         stack: List[Tuple[int, int]] = [(x, y)]
 
         while len(stack) > 0:
@@ -142,30 +176,30 @@ class MazeDrawer:
 
                 if chosen_neighbour == Direction.RIGHT:
                     self.__draw_maze_cell(x, y, Direction.RIGHT)
-                    self.__recursiveSolution[(x + CELL_DIMENSION, y)] = x, y
-                    x = x + CELL_DIMENSION
-                    visited.append((x, y))
+                    self.__recursiveSolution[(x + CELL_SIZE, y)] = x, y
+                    x = x + CELL_SIZE
+                    visited.add((x, y))
                     stack.append((x, y))
 
                 elif chosen_neighbour == Direction.LEFT:
                     self.__draw_maze_cell(x, y, Direction.LEFT)
-                    self.__recursiveSolution[(x - CELL_DIMENSION, y)] = x, y
-                    x = x - CELL_DIMENSION
-                    visited.append((x, y))
+                    self.__recursiveSolution[(x - CELL_SIZE, y)] = x, y
+                    x = x - CELL_SIZE
+                    visited.add((x, y))
                     stack.append((x, y))
 
                 elif chosen_neighbour == Direction.DOWN:
                     self.__draw_maze_cell(x, y, Direction.DOWN)
-                    self.__recursiveSolution[(x, y + CELL_DIMENSION)] = x, y
-                    y = y + CELL_DIMENSION
-                    visited.append((x, y))
+                    self.__recursiveSolution[(x, y + CELL_SIZE)] = x, y
+                    y = y + CELL_SIZE
+                    visited.add((x, y))
                     stack.append((x, y))
 
                 elif chosen_neighbour == Direction.UP:
                     self.__draw_maze_cell(x, y, Direction.UP)
-                    self.__recursiveSolution[(x, y - CELL_DIMENSION)] = x, y
-                    y = y - CELL_DIMENSION
-                    visited.append((x, y))
+                    self.__recursiveSolution[(x, y - CELL_SIZE)] = x, y
+                    y = y - CELL_SIZE
+                    visited.add((x, y))
                     stack.append((x, y))
 
             else:
@@ -173,27 +207,32 @@ class MazeDrawer:
                 # If all neighbouring cells are visited, remove the current one from the stack
                 x, y = stack.pop()
                 self.__draw_backtracking_cell(x, y)
-                time.sleep(.05)
 
-                # Change colour back after the backtracking has been displayed
-                self.__draw_maze_cell(x, y)
+        self.__mark_start_exit()
 
-        # Mark the entrance cell in a different colour. Do this when done drawing the maze due to how the backtracking cell overwrites colours in the cells it passes by.
-        self.__draw_maze_cell(x, y, None, Colour.GREEN)
+    def __mark_start_exit(self):
+        """
+        Convenience function for drawing the solution's start and exit positions as red and green cells, respectively.
+        """
 
-    def __draw_solution(self) -> None:
-        x: int = self.__solutionStart
-        y: int = self.__solutionStart
-        self.__draw_maze_cell(x, y, None, Colour.RED)
-        print(x)
-        print(y)
+        self.__draw_maze_cell(self.__solutionStartX, self.__solutionStartY, None, Colour.RED)
+        self.__draw_maze_cell(self.__rootX, self.__rootY, None, Colour.GREEN)
 
-        # Solution list contains all the coordinates to route back to start
-        self.__draw_solution_cell(x, y)
+    def __draw_recursive_solution(self) -> None:
+        """
+        Draws the recursive solution for the maze by following the key-value pairs back to the beginning. The key
+        corresponding to the current value was the previous step in the path. Updates (x, y) to equal the previous
+        cell, and continues retracing the steps until it reaches the maze's start.
+        :return:
+        """
+
+        x: int = self.__solutionStartX
+        y: int = self.__solutionStartY
 
         # Loop until cell position == Start position
         while (x, y) != (self.__rootX, self.__rootY):
-            # "key value" now becomes the new key
             x, y = self.__recursiveSolution[x, y]
             self.__draw_solution_cell(x, y)
             time.sleep(.1)
+
+
