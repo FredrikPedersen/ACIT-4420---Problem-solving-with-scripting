68563c789113eff3cf8a3c742a6f01f5b9ea7e2f
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-03 16:22:28 +0100

    Split ut recursive_solution if-block into separate convenience functions for readability. Added documentation.

--- Final Project/solution/MazeSolver.py
+++ Final Project/solution/MazeSolver.py
@@ -22,7 +22,7 @@ class MazeSolver:
 
     # ---------- General Solution Functions ---------- #
 
-    def solve_maze(self):
+    def solve_maze(self) -> None:
         self.__solutionSteps = []
         self.__mark_start_exit()
 
@@ -35,7 +35,7 @@ class MazeSolver:
 
         self.__draw_solution_cells()
 
-    def __mark_start_exit(self):
+    def __mark_start_exit(self) -> None:
         """
         Convenience function for drawing the solution's start and exit positions as red and green cells, respectively.
         """
@@ -67,8 +67,10 @@ class MazeSolver:
 
     def __recursive_build_solution(self) -> None:
         """
-        Draws the recursive solution for the maze by following the key-value pairs generated while building the maze
-        back to the beginning. The key corresponding to the current value was the previous step in the path. Updates
+        Creates a solution for the maze by following the key-value pairs generated while building the maze
+        back to the entrance.
+
+        The key corresponding to the current value was the previous step in the path. Updates
         (x, y) to equal the previous cell, and continues retracing the steps until it reaches the maze's start.
         """
 
@@ -83,24 +85,43 @@ class MazeSolver:
     # recursive_build_solution
 
     def __recursive_solution(self, x=__solutionStartX, y=__solutionStartY) -> bool:
+        """
+        Recursive path-finding solution to find the exit, prioritizing going left, right, up and then down.
+        Keeps going in a direction until a wall is found in the given direction or the given direction is outside the
+        maze's boundaries. Function terminates once the maze's entrance is found.
+
+        :param x: x-coordinate for current cell.
+        :param y: y-coordinate for current cell.
+        :return: true if there is another cell in a given direction, false if not. Also returns true when the exit is
+                found, or the current cell has already been visited.
+        """
 
         if (x, y) == (ROOT_X, ROOT_Y):
-            print("EXIT FOUND")
             return True
         elif self.__grid[x, y].visited_while_solving:
             return False
 
         self.__grid[x, y].visited_while_solving = True
 
-        if (not self.__grid[x, y].walls[Direction.LEFT] and x - CELL_SIZE > 0 and self.__recursive_solution(x - CELL_SIZE, y)) or (
-            not self.__grid[x, y].walls[Direction.RIGHT] and x + CELL_SIZE <= MAZE_WIDTH and self.__recursive_solution(x + CELL_SIZE, y)) or (
-            not self.__grid[x, y].walls[Direction.UP] and y - CELL_SIZE > 0 and self.__recursive_solution(x, y - CELL_SIZE)) or (
-            not self.__grid[x, y].walls[Direction.DOWN] and y + CELL_SIZE <= MAZE_HEIGHT and self.__recursive_solution(x, y + CELL_SIZE)):
+        if self.__check_left(x, y) or self.__check_right(x, y) or self.__check_up(x, y) or self.__check_down(x, y):
             self.__solutionSteps.append((x, y))
             return True
 
         return False
 
     # recursive_solution
+
+    # Convenience functions for readability in recursive_solution
+    def __check_left(self, x, y) -> bool:
+        return not self.__grid[x, y].walls[Direction.LEFT] and x - CELL_SIZE > 0 and self.__recursive_solution(x - CELL_SIZE, y)
+
+    def __check_right(self, x, y) -> bool:
+        return not self.__grid[x, y].walls[Direction.RIGHT] and x + CELL_SIZE <= MAZE_WIDTH and self.__recursive_solution(x + CELL_SIZE, y)
+
+    def __check_up(self, x, y) -> bool:
+        return not self.__grid[x, y].walls[Direction.UP] and y - CELL_SIZE > 0 and self.__recursive_solution(x, y - CELL_SIZE)
+
+    def __check_down(self, x, y) -> bool:
+        return not self.__grid[x, y].walls[Direction.DOWN] and y + CELL_SIZE <= MAZE_HEIGHT and self.__recursive_solution(x, y + CELL_SIZE)
 
     # ---------- A* Solution Functions ---------- #
