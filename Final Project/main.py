24b5d1f874f965b771dd5ae26cbdcba0741066eb
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-03 14:53:39 +0100

    Adjusted function calls and object creation in line with recent changes.

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,10 +1,12 @@
-import pygame
 from typing import Union
+
+import pygame
 from pygame.surface import SurfaceType, Surface
+
 from maze.MazeDrawer import MazeDrawer
+from solution.MazeSolver import MazeSolver
 from values.Constants import *
-from solution.Testing import Testing
-from maze.Grid import Grid
+from values.SolutionType import SolutionType
 
 # Initalise Pygame
 pygame.init()
@@ -12,14 +14,14 @@ pygame.mixer.init()
 screen: Union[Surface, SurfaceType] = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Maze Solver")
 
-mazeDrawer: MazeDrawer = MazeDrawer(screen, Grid.get_instance())
+mazeDrawer: MazeDrawer = MazeDrawer(screen)
 mazeDrawer.draw()
 
-#mazeSolver: MazeSolver = MazeSolver(screen, SolutionType.RECURSIVE, mazeDrawer.creation_steps)
-#mazeSolver.solve_maze()
+mazeSolver: MazeSolver = MazeSolver(screen, SolutionType.BUILD_SOLUTION, mazeDrawer.creation_steps)
+mazeSolver.solve_maze()
 
-solutionTests: Testing = Testing(screen, Grid.get_instance())
-solutionTests.solve_maze()
+# solutionTests: Testing = Testing(screen, Grid.get_instance())
+# solutionTests.solve_maze()
 
 
 # Pygame Loop
