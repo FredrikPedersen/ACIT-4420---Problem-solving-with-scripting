395347775b19c7294fe609cd7058d76fca6bd5b8
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-03 14:52:49 +0100

    Added new constants MAZE_WIDTH and MAZE_HEIGHT as to not have to multiply CELL_SIZE with the grids' dimensions whenever the size of the actual maze was needed.

--- Final Project/values/Constants.py
+++ Final Project/values/Constants.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 
 # Animations
-ANIMATIONS_ENABLED: bool = False
+ANIMATIONS_ENABLED: bool = True
 
 # Window Constants
 WINDOW_WIDTH: int = 600
@@ -10,11 +10,13 @@ WINDOW_HEIGHT: int = 600
 # Grid Constants
 GRID_WIDTH: int = 5
 GRID_HEIGHT: int = 5
-CELL_SIZE: int = 20
 
 # Maze Constants
 ROOT_X: int = 20
 ROOT_Y: int = 20
+CELL_SIZE: int = 20
+MAZE_WIDTH = GRID_WIDTH * CELL_SIZE
+MAZE_HEIGHT = GRID_HEIGHT * CELL_SIZE
 
 
 
