4abd441369c508ac67fb2b1591d93e1f1f4cc949
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-03 14:51:54 +0100

    Finised recursive solution logic

--- Final Project/solution/Testing.py
+++ Final Project/solution/Testing.py
@@ -5,13 +5,12 @@ import time
 
 
 class Testing:
-    __solutionStartX = GRID_WIDTH * CELL_SIZE
-    __solutionStartY = GRID_HEIGHT * CELL_SIZE
+    __solutionStartX = MAZE_WIDTH
+    __solutionStartY = MAZE_HEIGHT
 
     def __init__(self, screen: Union[Surface, SurfaceType], grid_instance: Grid):
         self.__screen = screen
         self.__grid = grid_instance.grid
-
     # init()
 
     # ---------- General Solution Functions ---------- #
@@ -27,7 +26,6 @@ class Testing:
 
         draw_maze_cell(self.__solutionStartX, self.__solutionStartY, self.__screen, None, Colour.RED)
         draw_maze_cell(ROOT_X, ROOT_Y, self.__screen, None, Colour.GREEN)
-
     # mark_start_exit()
 
     def __draw_solution_cell(self, x, y) -> None:
@@ -49,7 +47,7 @@ class Testing:
 
     # ---------- Recursive Solution Functions ---------- #
 
-    def __recursive_solution(self, x= GRID_WIDTH * CELL_SIZE, y=1 * CELL_SIZE) -> bool:
+    def __recursive_solution(self, x = MAZE_WIDTH, y = MAZE_HEIGHT) -> bool:
 
         if ANIMATIONS_ENABLED:
             time.sleep(.05)
@@ -64,13 +62,13 @@ class Testing:
             return False
 
         self.__grid[x, y].visited_while_solving = True
-        self.__draw_solution_cell(x, y)
         print(self.__grid[x, y].walls)
 
-        if (x < GRID_WIDTH * CELL_SIZE and self.__recursive_solution(x + CELL_SIZE, y)) or (
-                y > CELL_SIZE and self.__recursive_solution(x, y - CELL_SIZE)) or (
-                x > CELL_SIZE and self.__recursive_solution(x - CELL_SIZE, y)) or (
-                y < GRID_HEIGHT * CELL_SIZE and self.__recursive_solution(x, y + CELL_SIZE)):
+        if (not self.__grid[x, y].walls[Direction.LEFT] and x - CELL_SIZE > 0 and self.__recursive_solution(x - CELL_SIZE, y)) or (
+            not self.__grid[x, y].walls[Direction.RIGHT] and x + CELL_SIZE <= MAZE_WIDTH and self.__recursive_solution(x + CELL_SIZE, y)) or (
+            not self.__grid[x, y].walls[Direction.UP] and y - CELL_SIZE > 0 and self.__recursive_solution(x, y - CELL_SIZE)) or (
+            not self.__grid[x, y].walls[Direction.DOWN] and y + CELL_SIZE <= MAZE_HEIGHT and self.__recursive_solution(x, y + CELL_SIZE)):
+            self.__draw_solution_cell(x, y)
             return True
 
         return False
