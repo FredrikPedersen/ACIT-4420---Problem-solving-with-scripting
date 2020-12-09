fdba9b3683ad507961e1092987e3bc9a8c129d9f
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-09 17:44:29 +0100

    Added function for returning all SolutionTypes as a list

--- Final Project/values/SolutionType.py
+++ Final Project/values/SolutionType.py
@@ -6,3 +6,7 @@ class SolutionType(enum.Enum):
     BUILD_SOLUTION = "Build Solution"
     RECURSIVE = "Recursive"
     A_STAR = "A*"
+
+    @staticmethod
+    def as_list():
+        return list(map(lambda st: st.value, SolutionType))
