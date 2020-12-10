52448625454feacc1c42a03a981697115e6f6a60
Fredrik Pedersen <fredrikhp@gmail.com>
2020-12-10 18:27:41 +0100

    Added scales for solution start X and Y, with logic for restricting their max values to equal the selected width and height of the maze. Naming and redundancy cleanups.

--- Final Project/GUI.py
+++ Final Project/GUI.py
@@ -44,45 +44,49 @@ class Gui:
         maze_label = Label(window, text="Settings", font=("bold", 15))
         maze_label.grid(row=3, column=1, pady=10)
 
-        width_label = Label(window, text="Maze Width:", font=("bold", 10))
-        width_label.grid(row=4, column=1, pady=5)
-
         max_width: int = int(Constants.WINDOW_WIDTH/Constants.CELL_SIZE - 2)
         max_height: int = int(Constants.WINDOW_HEIGHT/Constants.CELL_SIZE - 2)
 
-        self.__scale_width = Scale(window, from_=5, to=max_width, orient=HORIZONTAL, command=self.__scale_width_callback)
-        self.__scale_width.grid(row=4, column=2)
+        Label(window, text="Maze Width:", font=("bold", 10)).grid(row=4, column=1, pady=5)
 
-        height_label = Label(window, text="Maze Height:", font=("bold", 10))
-        height_label.grid(row=5, column=1, pady=10)
+        self.__width_scale = Scale(window, from_=5, to=max_width, orient=HORIZONTAL, command=self.__width_scale_callback)
+        self.__width_scale.grid(row=4, column=2)
 
-        self.__scale_height = Scale(window, from_=5, to=max_height, orient=HORIZONTAL, command=self.__scale_height_callback)
-        self.__scale_height.grid(row=5, column=2)
+        Label(window, text="Maze Height:", font=("bold", 10)).grid(row=5, column=1, pady=10)
 
-        solution_option_label = Label(window, text="Solution Algorithm:", font=("bold", 10))
-        solution_option_label.grid(row=6, column=1, pady=10, padx=10)
+        self.__height_scale = Scale(window, from_=5, to=max_height, orient=HORIZONTAL, command=self.__height_scale_callback)
+        self.__height_scale.grid(row=5, column=2)
+
+        Label(window, text="Solution Algorithm:", font=("bold", 10)).grid(row=6, column=1, pady=10, padx=10)
+
         options = SolutionType.as_list()
         default_option = StringVar(window)
         default_option.set(options[0])
+
         self.__solution_option_menu = OptionMenu(window, default_option, *options, command=self.__option_menu_callback)
         self.__solution_option_menu.grid(row=6, column=2)
 
-        animations_label = Label(window, text="Draw Animations:", font=("bold", 10))
-        animations_label.grid(row=7, column=1, pady=10)
+        Label(window, text="Solution start X:", font=("bold", 10)).grid(row=7, column=1, pady=5)
+
+        self.__solution_start_x_scale = Scale(window, from_=5, to=self.__width_scale.get(), orient=HORIZONTAL)
+        self.__solution_start_x_scale.grid(row=7, column=2)
+
+        Label(window, text="Solution start Y:", font=("bold", 10)).grid(row=8, column=1, pady=10)
+
+        self.__solution_start_y_scale = Scale(window, from_=5, to=self.__height_scale.get(), orient=HORIZONTAL)
+        self.__solution_start_y_scale.grid(row=8, column=2)
+
+        Label(window, text="Draw Animations:", font=("bold", 10)).grid(row=9, column=1, pady=10)
+
         self.__animations_checked = BooleanVar()
         self.__animations_checked.set(True)
-        self.__animtations_check_button = Checkbutton(window, variable=self.__animations_checked, command=self.__animtations_check_callback).grid(row=7, column=2)
+        self.__animations_check_button = Checkbutton(window, variable=self.__animations_checked, command=self.__animations_check_callback).grid(row=9, column=2)
 
         # ---------- Actions ---------- #
 
-        solution_label = Label(window, text="Actions", font=("bold", 15))
-        solution_label.grid(row=8, column=1, pady=10)
-
-        draw_button = Button(window, text="Draw Maze", command=self.__draw_maze)
-        draw_button.grid(row=9, column=1, pady=10)
-
-        solve_button = Button(window, text="Solve Maze", command=self.__solve_maze)
-        solve_button.grid(row=9, column=2, pady=10)
+        Label(window, text="Actions", font=("bold", 15)).grid(row=10, column=1, pady=10)
+        Button(window, text="Draw Maze", command=self.__draw_maze).grid(row=11, column=1, pady=10)
+        Button(window, text="Solve Maze", command=self.__solve_maze).grid(row=11, column=2, pady=10)
 
     # initialize_inputs()
 
@@ -121,15 +125,17 @@ class Gui:
 
         raise Exception("A non-existing solution type has been selected. This should not be able to happen.")
 
-    def __scale_width_callback(self, value):
+    def __width_scale_callback(self, value):
         Constants.set_grid_width(int(value))
+        self.__solution_start_x_scale.configure(to=value)
 
-    def __scale_height_callback(self, value):
+    def __height_scale_callback(self, value):
         Constants.set_grid_height(int(value))
+        self.__solution_start_y_scale.configure(to=value)
 
-    def __animtations_check_callback(self):
+    def __animations_check_callback(self):
         Constants.ANIMATIONS_ENABLED = self.__animations_checked.get()
 
     def __clear_pygame_screen(self):
-        self.__screen.fill(Colour.BLACK)
+        self.__screen.fill(Colour.BLACK.value)
         pygame.display.update()
